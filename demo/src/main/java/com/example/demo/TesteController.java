b6629a9e655276af1994185b3605a8578c8380fa
Kauana-bm24 <kauana.bm24@aluno.ifsc.edu.br>
2025-10-07 22:20:20 -0300

    demo2

--- demo/src/main/java/com/example/demo/TesteController.java
+++ demo/src/main/java/com/example/demo/TesteController.java
@@ -1,8 +1,16 @@
-package com.exempole.demo;
+package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestController;
 
 @RestController
+@RequestMapping("/")
 public class TesteController {
+
+    @GetMapping
+    public String ola() {
+        
+                return "Hello, World!";
+    }
+
 }
